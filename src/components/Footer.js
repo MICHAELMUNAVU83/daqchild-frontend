6585793939c71c99cc39606a23f5452fcd82ff79
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-02-06 20:00:16 +0300

    Added contacts and links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,57 +10,80 @@ import { SiTwitch } from "react-icons/si";
 
 const Footer = () => {
   return (
-    <div className="bg-black h-full text-white p-4">
+    <div className="bg-black h-full text-white md:p-4 p-2">
       <p className="text-center p-3 text-4xl">Follow me</p>
       <section className="w-full md:w-full  flex-wrap flex bg-dark   justify-center items-center ">
         <a
           href="https://www.instagram.com/"
-          className="border-white border-2 p-4 group hover:bg-white duration-300  ease-in-out  cursor-pointer   "
+          className="border-white border-2 md:p-4 p-2 group hover:bg-white duration-300  ease-in-out  cursor-pointer   "
         >
           <AiFillInstagram className="text-4xl group-hover:text-black group-hover:text-black hover:scale-110 duration-300 mx-2 cursor-pointer " />
         </a>
         <a
           href="https://www.instagram.com/"
-          className="border-white border-2 p-4 group hover:bg-white duration-300  ease-in-out  cursor-pointer   "
+          className="border-white border-2 md:p-4 p-2 group hover:bg-white duration-300  ease-in-out  cursor-pointer   "
         >
           <AiFillFacebook className="text-4xl group-hover:text-black   group-hover:scale-110 duration-300 mx-2 cursor-pointer " />
         </a>
         <a
           href="https://www.instagram.com/"
-          className="border-white border-2 p-4 group hover:bg-white duration-300  ease-in-out  cursor-pointer   "
+          className="border-white border-2 md:p-4 p-2 group hover:bg-white duration-300  ease-in-out  cursor-pointer   "
         >
           <AiFillTwitterCircle className="text-4xl  group-hover:text-black group-hover:scale-110 duration-300 mx-2 cursor-pointer " />
         </a>
         <a
           href="https://www.instagram.com/"
-          className="border-white border-2 p-4 group hover:bg-white duration-300  ease-in-out  cursor-pointer   "
+          className="border-white border-2 md:p-4 p-2 group hover:bg-white duration-300  ease-in-out  cursor-pointer   "
         >
           <FaTiktok className="text-4xl  group-hover:text-black  group-hover:scale-110 duration-300 mx-2 cursor-pointer " />
         </a>
         <a
           href="https://www.instagram.com/"
-          className="border-white border-2 p-4 group hover:bg-white duration-300  ease-in-out  cursor-pointer   "
+          className="border-white border-2 md:p-4 p-2 group hover:bg-white duration-300  ease-in-out  cursor-pointer   "
         >
           <BsApple className="text-4xl group-hover:text-black   group-hover:scale-110 duration-300 mx-2 cursor-pointer " />
         </a>
         <a
           href="https://www.instagram.com/"
-          className="border-white border-2 p-4 group hover:bg-white duration-300  ease-in-out  cursor-pointer   "
+          className="border-white border-2 md:p-4 p-2 group hover:bg-white duration-300  ease-in-out  cursor-pointer   "
         >
           <BsSpotify className="text-4xl group-hover:text-black  group-hover:scale-110 duration-300  mx-2 cursor-pointer " />
         </a>
         <a
           href="https://www.instagram.com/"
-          className="border-white border-2 p-4 group hover:bg-white duration-300  ease-in-out  cursor-pointer   "
+          className="border-white border-2 md:p-4 p-2 group hover:bg-white duration-300  ease-in-out  cursor-pointer   "
         >
           <BsSnapchat className="text-4xl group-hover:text-black group-hover:scale-110 duration-300 mx-2 cursor-pointer" />
         </a>
         <a
           href="https://www.instagram.com/"
-          className="border-white border-2 p-4 group hover:bg-white duration-300  ease-in-out  cursor-pointer   "
+          className="border-white border-2 md:p-4 p-2 group hover:bg-white duration-300  ease-in-out  cursor-pointer   "
         >
           <SiTwitch className="text-4xl group-hover:text-black   group-hover:scale-110 duration-300 mx-2 cursor-pointer " />
         </a>
+      </section>
+
+      <section className="flex flex-col mt-10 gap-10 md:flex-row justify-center items-center">
+        <div className="flex flex-col justify-center items-center ">
+          <p className="text-center p-3 text-4xl">Contact</p>
+          <p className="text-center p-1  text-gray-400 ">Email: </p>
+          <p className="text-center p-1  text-gray-400  ">Phone: </p>
+        </div>
+        <div className="flex flex-col justify-center items-center">
+          <p className="text-center p-3 text-4xl">Contact</p>
+          <p className="text-center p-1  text-gray-400  ">Email: </p>
+          <p className="text-center p-1  text-gray-400  ">Phone: </p>
+        </div>
+        <div className="flex flex-col justify-center items-center">
+          <p className="text-center p-3 text-4xl">Contact</p>
+          <p className="text-center p-1  text-gray-400  ">Email: </p>
+          <p className="text-center p-1  text-gray-400  ">Phone: </p>
+        </div>
+        <div className="flex flex-col justify-center items-center">
+          <p className="text-center p-3 text-4xl">Contact</p>
+          <p className="text-center p-1  text-gray-400 ">Email: </p>
+          <p className="text-center p-1  text-gray-400  ">Phone: </p>
+        </div>
       </section>
     </div>
   );
