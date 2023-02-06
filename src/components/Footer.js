72f0d250242af644a470f2fb9526aeca165ca756
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-02-06 13:45:34 +0300

    Started footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from "react";
+import { AiFillInstagram, AiFillFacebook, AiFillTwitterCircle } from "react-icons/ai";
+import { FaTiktok } from "react-icons/fa";
+import { BsSnapchat, BsSpotify, BsApple } from "react-icons/bs";
+import { SiTwitch } from "react-icons/si";
+
+
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="bg-black h-full text-white p-4">
+      <p>Follow me</p>
+          <section className="w-80 flex bg-dark   justify-center items-center mx-auto">
+              <a href="https://www.instagram.com/" className="border-white border-2  ">
+                  <AiFillInstagram className="text-4xl  mx-2" />
+              </a>
+              <a href="https://www.instagram.com/"  className="border-white border-2 border-l-0  ">
+                  <AiFillInstagram className="text-4xl  mx-2" />
+              </a>
+              <a href="https://www.instagram.com/"  className="border-white border-2 border-l-0  ">
+                  <AiFillInstagram className="text-4xl  mx-2" />
+              </a>
+              <a href="https://www.instagram.com/"  className="border-white border-2 border-l-0  ">
+                  <AiFillInstagram className="text-4xl  mx-2" />
+              </a>
+              <a href="https://www.instagram.com/"  className="border-white border-2 border-l-0  ">
+                  <AiFillInstagram className="text-4xl  mx-2" />
+              </a>
+              <a href="https://www.instagram.com/"  className="border-white border-2 border-l-0  ">
+                  <AiFillInstagram className="text-4xl  mx-2" />
+              </a>
+              <a href="https://www.instagram.com/"  className="border-white border-2 border-l-0  ">
+                  <AiFillInstagram className="text-4xl  mx-2" />
+              </a>
+              <a href="https://www.instagram.com/"  className="border-white border-2 border-l-0  ">
+                  <AiFillInstagram className="text-4xl  mx-2" />
+              </a>
+            
+              
+             
+              
+      </section>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
