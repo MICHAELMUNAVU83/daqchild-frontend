a1e325c4f286f760338bf79b52f8097b8a19c98e
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-02-09 06:35:22 +0300

    Added animation

--- src/components/HorizontalText.js
+++ src/components/HorizontalText.js
@@ -18,7 +18,6 @@ const HorizontalText = () => {
     changeImage();
   }, [current]);
 
-
   return (
     <div className="md:h-[100vh] h-[80vh]  bg-black text-white overflow-hidden w-full">
       <div>
@@ -29,7 +28,30 @@ const HorizontalText = () => {
             className=" md:w-[60%] w-80 absolute md:h-[80vh]  md:h-[80vh] object-cover"
           />
         </div>
-        <section className="pt-20 flex flex-col h-full justify-center align-center relative text-3xl md:text-5xl font-bold ">
+
+        <div className="scroll-container">
+          <p className="flex scroll-text text-6xl">
+            <span>GIBBZ THE DAQCHILD</span>
+            <span className="mx-2">KING OF GOOD TIMES </span>
+          </p>
+        </div>
+
+        <div className="middle-scroll-container">
+          <p className="flex middle-scroll-text text-6xl">
+            <span>AFROBEATS || AMAPAINO || REAGEE </span>
+            <span className="mx-2">HIPHOP || BONGO || GENGETONE </span>
+          </p>
+        </div>
+
+
+        <div className="last-scroll-container">
+          <p className="flex last-scroll-text text-6xl">
+            <span>GIBBZ THE DAQCHILD</span>
+            <span className="mx-2">KING OF GOOD TIMES </span>
+          </p>
+        </div>
+
+        {/* <section className="pt-20 flex flex-col h-full justify-center align-center relative text-3xl md:text-5xl font-bold ">
           <div className="marquee-w">
             <div className="marquee">
               <span>GIBBZ THE DAQCHILD</span>
@@ -54,7 +76,7 @@ const HorizontalText = () => {
               <span>THE KING OF GOOD TIMES</span>
             </div>
           </div>
-        </section>
+        </section> */}
       </div>
     </div>
   );
