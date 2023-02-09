92e515c9c620a8341c9a2a6223497fc2b35c2e88
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-02-09 15:39:07 +0300

    Setup

--- src/components/AddMix.js
+++ src/components/AddMix.js
@@ -7,23 +7,25 @@ function AddMix() {
 
     formData.append("file", files[0]);
     formData.append("upload_preset", "e2e6z2lx");
-    fetch("https://api.cloudinary.com/v1_1/dakiak4mc/audio/upload", {
+    fetch("https://api.cloudinary.com/v1_1/dakiak4mc/video/upload", {
       method: "POST",
       body: formData,
     })
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
+        setAudio(data.secure_url);
       });
   };
   return (
     <div className="App">
       <input type="file" onChange={(e) => uploadImage(e.target.files)} />
 
-          {/* <audio controls>
-              <source src={audio} type="audio/mp3" />
-          </audio> */}
-          
+      <audio controls autoPlay>
+        <source
+          src="https://res.cloudinary.com/dakiak4mc/video/upload/v1675945950/vmo2lxodl3e5vdat57nx.mp3"
+          type="audio/mp3"
+        />
+      </audio>
     </div>
   );
 }
