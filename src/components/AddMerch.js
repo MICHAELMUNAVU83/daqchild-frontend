03fb944db850ed14202afb1bbda04803f60c1635
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-02-15 18:58:52 +0300

    Styled add merch

--- src/components/AddMerch.js
+++ src/components/AddMerch.js
@@ -41,33 +41,158 @@ const AddMerch = () => {
   };
 
   return (
+    // <div className="mt-20">
+    //   <form className="flex flex-col gap-4" onSubmit={addMerchFunctionality}>
+    //     <input
+    //       type="text"
+    //       placeholder="Name"
+    //       value={name}
+    //       onChange={(e) => setName(e.target.value)}
+    //     />
+    //     <input
+    //       type="integer"
+    //       placeholder="Price"
+    //       value={price}
+    //       onChange={(e) => setPrice(e.target.value)}
+    //     />
+
+    //     <input
+    //       type="text"
+    //       placeholder="Description"
+    //       value={description}
+    //       onChange={(e) => setDescription(e.target.value)}
+    //     />
+
+    //     <input type="file" onChange={(e) => uploadImage(e.target.files)} />
+    //     <button className="bg-black text-white p-4" type="submit">
+    //       Add Merch
+    //     </button>
+    //   </form>
+    // </div>
     <div className="mt-20">
-      <form className="flex flex-col gap-4" onSubmit={addMerchFunctionality}>
-        <input
-          type="text"
-          placeholder="Name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <input
-          type="integer"
-          placeholder="Price"
-          value={price}
-          onChange={(e) => setPrice(e.target.value)}
-        />
+      <div className="md:max-w-[50%] w-[80%] mx-auto py-6 sm:px-6 lg:px-8">
+        <div className="mt-5 md:col-span-2 md:mt-0">
+          <form onSubmit={addMerchFunctionality}>
+            <div className="shadow sm:overflow-hidden sm:rounded-md">
+              <div className="space-y-6 bg-white px-4 py-5 sm:p-6">
+                <div>
+                  <label
+                    for="about"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Name
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      type={"text"}
+                      className=" border border-gray-300  text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+                      placeholder="Hoodies, T-shirts, etc."
+                      value={name}
+                      onChange={(e) => setName(e.target.value)}
+                    />
+                  </div>
+                  <p className="mt-2 text-sm text-gray-500">
+                    Give the name of the merchaandise.
+                  </p>
+                </div>
 
-        <input
-          type="text"
-          placeholder="Description"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-        />
+                <div>
+                  <label
+                    for="about"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Description
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      type={"text"}
+                      className=" border border-gray-300  text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+                      value={description}
+                      placeholder="The jumper has high quality material and has many pockets."
+                      onChange={(e) => setDescription(e.target.value)}
+                    />
+                  </div>
+                  <p className="mt-2 text-sm text-gray-500">
+                    Add a description of the merchandise on sale .
+                  </p>
+                </div>
+                <div>
+                  <label
+                    for="about"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Price
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      type={"number"}
+                      className=" border border-gray-300  text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+                      value={price}
+                      placeholder="2500, 5000, etc."
+                      onChange={(e) => setPrice(e.target.value)}
+                    />
+                  </div>
+                  <p className="mt-2 text-sm text-gray-500">
+                    Add a price for the merchandise.
+                  </p>
+                </div>
 
-        <input type="file" onChange={(e) => uploadImage(e.target.files)} />
-        <button className="bg-black text-white p-4" type="submit">
-          Add Merch
-        </button>
-      </form>
+                <div>
+                  <label className="block text-sm font-medium text-gray-700">
+                    Merchandise Image
+                  </label>
+                  <div className="mt-1 flex justify-center rounded-md border-2 border-dashed border-gray-300 px-6 pt-5 pb-6">
+                    <div className="space-y-1 text-center">
+                      <svg
+                        className="mx-auto h-12 w-12 text-gray-400"
+                        stroke="currentColor"
+                        fill="none"
+                        viewBox="0 0 48 48"
+                        aria-hidden="true"
+                      >
+                        <path
+                          d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+                          stroke-width="2"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                        />
+                      </svg>
+                      <div className="flex text-sm text-gray-600">
+                        <label
+                          for="file-upload"
+                          className="relative cursor-pointer rounded-md bg-white font-medium text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:text-indigo-500"
+                        >
+                          <span>Add an image</span>
+                          <input
+                            id="file-upload"
+                            name="file-upload"
+                            type="file"
+                            className="sr-only"
+                            onChange={(e) => uploadImage(e.target.files)}
+                          />
+                        </label>
+                        <p className="pl-1">or drag and drop</p>
+                      </div>
+                      <p className="text-xs text-gray-500">
+                        PNG, JPG, GIF up to 10MB
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="bg-gray-50 flex justify-center px-4 py-3 text-right sm:px-6">
+                <button
+                  type="submit"
+                  className=" text-center rounded-md border border-transparent bg-indigo-600 py-2 px-4  font-bold text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                >
+                  Add Merchandise
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
