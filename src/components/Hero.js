aae6c37192193a7e41ca6523807fcc6f383b79d0
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-02-16 07:50:13 +0300

    Changed img

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,8 +3,8 @@ import { AiOutlineCaretDown } from "react-icons/ai";
 const Hero = () => {
   return (
     <div className="hero-background md:h-[100vh] h-[70vh] bg-center  md:bg-top   bg-fixed bg-no-repeat bg-cover ">
-      <div className="bg-black/60  h-full">
-        <div className="flex flex-col justify-center text-center items-center h-full">
+      <div className="bg-black/30  h-full">
+        <div className="flex pt-20 flex-col justify-center text-center items-center h-full">
           <h1
             className="md:text-8xl text-5xl text-white font-bold"
             style={{
@@ -17,13 +17,9 @@ const Hero = () => {
           </h1>
           <p className="text-2xl text-white font-bold">KING OF GOOD TIMES</p>
 
-          <button className="bg-white uppercase text-black font-bold py-4 px-8  mt-4">
-            Explore
-          </button>
+          
 
-          <div className="flex justify-center items-center mt-10">
-            <AiOutlineCaretDown className="text-8xl text-pink-300 animate-bounce" />
-          </div>
+          
           <div className="scroll-container">
             <p className="flex scroll-text text-4xl md:text-7xl">
               <span>GIBBZ THE DAQCHILD&nbsp;&nbsp;&nbsp;</span>
