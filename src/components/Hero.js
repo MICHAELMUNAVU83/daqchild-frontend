904a770912638d140c22191cab5ae78cbf71524a
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-02-16 12:55:51 +0300

    Changed img

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { AiOutlineCaretDown } from "react-icons/ai";
+
 const Hero = () => {
   return (
-    <div className="hero-background md:h-[100vh] h-[70vh] bg-center  md:bg-top   bg-fixed bg-no-repeat bg-cover ">
+    <div className="hero-background md:h-[100vh] h-[70vh] bg-top    bg-fixed bg-no-repeat bg-cover ">
       <div className="bg-black/30  h-full">
         <div className="flex pt-20 flex-col justify-center text-center items-center h-full">
           <h1
-            className="md:text-8xl text-5xl text-white font-bold"
+            className="md:text-8xl text-4xl text-white font-bold"
             style={{
               WebkitTextStroke: "2px white",
               WebkitTextFillColor: "transparent",
@@ -17,16 +17,14 @@ const Hero = () => {
           </h1>
           <p className="text-2xl text-white font-bold">KING OF GOOD TIMES</p>
 
-          
-
-          
-          <div className="scroll-container">
-            <p className="flex scroll-text text-4xl md:text-7xl">
-              <span>GIBBZ THE DAQCHILD&nbsp;&nbsp;&nbsp;</span>
-              <span className="mx-2">KING OF GOOD TIMES &nbsp;&nbsp;&nbsp;</span>
-            </p>
+          <div className="marquee-w text-4xl md:text-7xl text-white font-bold">
+            <div className="marquee">
+              <span>DEEJAY GIBBZ THE DAQCHILD&nbsp;&nbsp;&nbsp;</span>
+            </div>
+            <div className="marquee marquee2">
+              <span>&nbsp; THE KING OF GOOD TIMES &nbsp;&nbsp;&nbsp;</span>
+            </div>
           </div>
-         
         </div>
       </div>
     </div>
